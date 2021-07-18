adce2393f0d176fdb397e8d07b131a4df39cb6d1
MASTREX <38161604+MASTREX@users.noreply.github.com>
2021-07-18 20:59:26 +0530

    Implement logger

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from time import sleep
+from os import system
+import logging
+
 import requests
 from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup
-from time import sleep
-from os import system
 
 class FlipkartTracker():
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
     def __init__(self, url, limit = 0):
+        self.logger = logging.getLogger()
         self.url = url
         self.is_active = False
         self.limit = limit
@@ -29,7 +32,9 @@ class FlipkartTracker():
         self.session.verify = 'PortSwiggerCA.crt'
 
     def fetch(self):
+        self.logger.debug('Making request...')
         response = self.session.get(self.url)
+        self.logger.debug('Got Response')
         soup = BeautifulSoup(response.content, 'html.parser')
         price = soup.find('div', attrs={'class': '_30jeq3 _16Jk6d'}).text
         self.price = (int)(price[1:].replace(',', ''))
@@ -52,7 +57,14 @@ class FlipkartTracker():
     def end(self):
         self.session.close()
 
-if(__name__ == '__main__'):
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    c_handler = logging.StreamHandler()
+    c_handler.setFormatter(logging.Formatter('%(asctime)s : %(name)s - %(levelname)s : %(message)s'))
+    c_handler.setLevel(logging.INFO)
+    logger.addHandler(c_handler)
+
     products_dict = {
         #'Name': tracker
         'Samsung Monitor': FlipkartTracker(url='https://www.flipkart.com/samsung-23-8-inch-curved-full-hd-led-backlit-va-panel-monitor-lc24f390fhwxxl/p/itmezu53yhwg2ayg?pid=MONEZU4Z8BYBV2GZ', limit=9000),
@@ -68,16 +80,19 @@ if(__name__ == '__main__'):
             for name, tracker in products_dict.items():
                 tracker.fetch()
                 status_str = name + ': ' + tracker.status()
-                print(status_str)
-                system('termux-notification -c \'' + status_str + '\' --group flipkart -i 0 --title \'Flipkart Tracker\'')
+                logger.info(status_str)
+                #system('termux-notification -c \'' + status_str + '\' --group flipkart -i 0 --title \'Flipkart Tracker\'')
+                logger.warning('sleeping for {}'.format(sleep_time))
                 sleep(sleep_time)
         except ConnectionError:
-            print('No Internet!')
+            logger.error('No Internet!')
             sleep(10*60)
         except KeyboardInterrupt:
-            print('Terminated by user')
+            logger.error('Terminated by user')
             break
         except Exception as e:
+            logger.exception(e)
             raise e
     for tracker in products_dict.values():
         tracker.end()
+        logger.debug('all session closed successfully')
